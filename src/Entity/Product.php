30d85e9bb72ee6a0666bd5b240527cf7a06e6811
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-04-11 06:59:22 -0300

    adicionando extensão e setters

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,17 +3,18 @@
 namespace App\Entity;
 
 use App\Entity\ProductType;
+use App\Entity\Entity;
+use App\Repository\ProductRepository;
 
-class Product
+class Product extends Entity
 {
-  private ?int $id = null;
   private ?string $name = null;
   private ?float $price = null;
   private ?ProductType $productType = null;
 
-  public function getId(): ?int
+  function __construct()
   {
-    return $this->id;
+    $this->repository = new ProductRepository();
   }
 
   public function getName(): ?string
@@ -29,5 +30,30 @@ class Product
   public function getProductType(): ?ProductType
   {
     return $this->productType;
+  }
+
+  public function setName(string $name)
+  {
+    $this->name = $name;
+  }
+
+  public function setPrice(float $price)
+  {
+    $this->price = $price;
+  }
+
+  public function setProductType(ProductType $productType)
+  {
+    $this->productType = $productType;
+  }
+
+  public function create()
+  {
+    $this->repository->insert($this);
+  }
+
+  public function listAll()
+  {
+    
   }
 }
